69febb3e2efde031ad4bd17fb8f31dd421b30d61
Yukiya Adachi <yukiacra954@yahoo.co.jp>
2018-02-03 23:41:35 +0900

    second commit

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -3,10 +3,38 @@ class TitlesController < ApplicationController
     @titles = Title.all
   end
 
+
+
   def show
     @title = Title.find_by(id: params[:id])
     @posts = Post.where(title_id: params[:id])
+    @start_num = 0
+    @end_num = @posts.length
+    
+    if params[:range]
+      if params[:range] == "latest50"
+        @start_num = @posts.length - 50
+        @end_num = @posts.length
+      else
+        @start_num = params[:range].split("~")[0].to_i
+        @end_num = params[:range].split("~")[1].to_i
+      end
+    end
+
+    if @start_num < 0
+      @start_num = 0
+      redirect_to("/titles/#{params[:id]}/show/#{@start_num}~#{@end_num}")
+    end
+
+    if @end_num > @posts.length
+      @end_num = @posts.length
+      redirect_to("/titles/#{params[:id]}/show/#{@start_num}~#{@end_num}")
+    end
+
+    @posts = @posts[@start_num, @end_num]
   end
+
+
 
   def create
     @title = Title.new(name: params[:name])
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'titles/index'
   get 'titles/:id/show' => 'titles#show'
+  get 'titles/:id/show/:range' => 'titles#show'
   post 'titles/new' => 'titles#create' 
 
   get '/' => 'home#top'
